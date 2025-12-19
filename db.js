c3f85319f48eea098a83a583a40137331792a14a
manpreetsingh1151 <manpreetsingh1151@yahoo.com>
2025-12-19 22:45:23 +0000

    Fix MongoDB env connection

--- db.js
+++ db.js
@@ -6,10 +6,18 @@ let isConnected = false;
 
 module.exports = async () => {
   if (isConnected) return;
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
 
 // mongoose.connect("mongodb+srv://manpreetsingh1151:Ghotra%401315@cluster0.bsg7myf.mongodb.net/?appName=Cluster0")
-mongoose.connect("process.env.MONGO_URI")
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error("MongoDB connection error:", err));
+// mongoose.connect("process.env.MONGO_URI")
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch(err => console.error("MongoDB connection error:", err));
 
-};
+// };
