e852ebadf0064486124ad8bcbc72460289bbe308
manpreetsingh1151 <manpreetsingh1151@yahoo.com>
2025-12-19 19:19:49 +0000

    made changes for vercel deployment

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.post("/submit", async (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-})
+module.exports = app;
+
+// app.listen(3000, () => {
+//     console.log("Server running on port 3000");
+// })
--- db.js
+++ db.js
@@ -2,6 +2,7 @@
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://manpreetsingh1151:Ghotra%401315@cluster0.bsg7myf.mongodb.net/?appName=Cluster0")
+// mongoose.connect("mongodb+srv://manpreetsingh1151:Ghotra%401315@cluster0.bsg7myf.mongodb.net/?appName=Cluster0")
+mongoose.connect("process.env.MONGO_URI")
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
